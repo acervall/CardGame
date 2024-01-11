7d167edfbb4ddf359a56c4d4e1e57338a0480872
Amelie Cervall <ameliecervall@gmail.com>
2024-01-11 14:34:36 +0100

    coverage

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import istanbul from 'vite-plugin-istanbul'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    istanbul({
+      cypress: true,
+      requireEnv: false,
+    }),
+  ],
   server: {
+    host: true,
+    port: 3000,
     proxy: {
       '/api/': {
         target: 'http://localhost:3000/',
