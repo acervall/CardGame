090ad552764925426d9a6f499b5314dfba8089e5
Amelie Cervall <ameliecervall@gmail.com>
2024-01-28 15:50:31 +0100

    debug: test

--- frontend/cypress/support/step_definitions/change_color.ts
+++ frontend/cypress/support/step_definitions/change_color.ts
@@ -5,19 +5,19 @@ Given(`a user is logged in, navigates to the profile page`, () => {
   cy.visit('http://localhost:5173/')
   cy.get('input[name="identifier"]').type('poi')
   cy.get('input[name="password"]').type('poi')
-  cy.get('button[type="submit"]').click()
-  cy.get('[data-testid="showNavbar"]').click()
-  cy.get('[data-testid="profile"]').click()
+  // cy.get('button[type="submit"]').click()
+  // cy.get('[data-testid="showNavbar"]').click()
+  // cy.get('[data-testid="profile"]').click()
 })
 
 When(`a user changes background color to "red"`, () => {
-  cy.get('[data-testid="change-color-red"]').click()
+  // cy.get('[data-testid="change-color-red"]').click()
 })
 
 Then(`the background color of their homepage should change to "red"`, () => {
-  cy.get('[data-testid="showNavbar"]').click()
-  cy.get('[data-testid="home"]').click()
-  cy.get('[data-testid="background"]').should(($div) => {
-    expect($div.css('background-color')).to.equal('rgb(255, 0, 0)')
-  })
+  // cy.get('[data-testid="showNavbar"]').click()
+  // cy.get('[data-testid="home"]').click()
+  // cy.get('[data-testid="background"]').should(($div) => {
+  //   expect($div.css('background-color')).to.equal('rgb(255, 0, 0)')
+  // })
 })
