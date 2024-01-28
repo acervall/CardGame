026608c8d0031d2e6ad99f973d315ffdd392ef9a
Amelie Cervall <ameliecervall@gmail.com>
2024-01-28 16:09:00 +0100

    dubug: updating paths

--- frontend/cypress/support/step_definitions/change_color.ts
+++ frontend/cypress/support/step_definitions/change_color.ts
@@ -5,13 +5,13 @@ Given(`a user is logged in, navigates to the profile page`, () => {
   cy.visit('http://localhost:5173/')
   cy.get('input[name="identifier"]').type('poi')
   cy.get('input[name="password"]').type('poi')
-  // cy.get('button[type="submit"]').click()
-  // cy.get('[data-testid="showNavbar"]').click()
-  // cy.get('[data-testid="profile"]').click()
+  cy.get('button[type="submit"]').click()
+  cy.get('[data-testid="showNavbar"]').click()
+  cy.get('[data-testid="profile"]').click()
 })
 
 When(`a user changes background color to "red"`, () => {
-  // cy.get('[data-testid="change-color-red"]').click()
+  cy.get('[data-testid="change-color-red"]').click()
 })
 
 Then(`the background color of their homepage should change to "red"`, () => {
