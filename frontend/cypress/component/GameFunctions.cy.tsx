091ca09e32414c6b1e8e041f15f8b1f966747aa6
Amelie Cervall <ameliecervall@gmail.com>
2024-01-29 18:53:41 +0100

    component tests

--- frontend/cypress/component/GameFunctions.cy.tsx
+++ frontend/cypress/component/GameFunctions.cy.tsx
@@ -10,28 +10,28 @@ const withGameProviders = (component: ReactNode) => {
 }
 
 describe('test game functions', () => {
-  // beforeEach(() => {
-  //   cy.intercept('GET', '/auth/info', { fixture: 'user.json' }).as('getUser')
-  // })
-  // it('renders', () => {
-  //   const socket = io(BASE_URL)
-  //   cy.mount(withGameProviders(<Sequence />))
-  //   cy.wait('@getUser')
-  //   cy.wrap(
-  //     new Promise((resolve) => {
-  //       socket.emit('initGame', { color: 'red', username: 'poi' })
-  //       socket.emit('initGame', { color: 'green', username: 'testUser' })
-  //       socket.emit('startGame')
-  //       socket.on('gameState', (gameState) => {
-  //         console.log('gameState', gameState)
-  //         resolve(gameState)
-  //       })
-  //     }),
-  //   ).then(() => {
-  //     cy.get('[data-testid="cards"]').should('have.length', 7)
-  //     cy.get('[data-testid="cards"]').first().click()
-  //     cy.get('[data-card-status="Available"]').first().click()
-  //     cy.get('[data-testid="draw"]').click()
-  //   })
-  // })
+  beforeEach(() => {
+    cy.intercept('GET', '/auth/info', { fixture: 'user.json' }).as('getUser')
+  })
+  it('renders', () => {
+    const socket = io(BASE_URL)
+    cy.mount(withGameProviders(<Sequence />))
+    cy.wait('@getUser')
+    cy.wrap(
+      new Promise((resolve) => {
+        socket.emit('initGame', { color: 'red', username: 'poi' })
+        socket.emit('initGame', { color: 'green', username: 'testUser' })
+        socket.emit('startGame')
+        socket.on('gameState', (gameState) => {
+          console.log('gameState', gameState)
+          resolve(gameState)
+        })
+      }),
+    ).then(() => {
+      cy.get('[data-testid="cards"]').should('have.length', 7)
+      cy.get('[data-testid="cards"]').first().click()
+      cy.get('[data-card-status="Available"]').first().click()
+      cy.get('[data-testid="draw"]').click()
+    })
+  })
 })
